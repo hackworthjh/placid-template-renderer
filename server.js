5c72f0fdd7ebe4a5a9fcc286383844a919d97397
hackworthjh <hackworth.jh@gmail.com>
2026-01-02 13:22:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ WrapStyle: 2
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, BackColour, Bold, Italic, Alignment, MarginL, MarginR, MarginV, BorderStyle, Outline, Shadow
-Style: Default,Arial,56,&H00FFFFFF,&HA0000000,0,0,5,120,120,260,3,2,0
+Style: Default,Arial,54,&H00FFFFFF,&H80000000,0,0,2,140,140,180,3,0,0
 
 [Events]
 Format: Layer, Start, End, Style, Text
