4c81c11916d11952395251ae24dae38958a9d206
hackworthjh <hackworth.jh@gmail.com>
2026-02-10 22:57:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ ${events}
 
 const renderCmd = `
 ffmpeg -y -i base.mp4 -i audio.mp3 \
--vf "scale=${VIDEO_W}:${VIDEO_H},drawbox=x=0:y=0:w=${VIDEO_W}:h=${VIDEO_H}:t=${BORDER}:color=white,subtitles=captions.ass" \
+-vf "scale=${VIDEO_W}:${VIDEO_H},drawbox=x=0:y=0:w=${VIDEO_W}:h=${VIDEO_H}:t=${BORDER}:color=grey,subtitles=captions.ass" \
 -map 0:v -map 1:a -shortest \
 -c:v libx264 -preset ultrafast -crf 23 \
 -c:a aac -b:a 192k -pix_fmt yuv420p "${output}"
